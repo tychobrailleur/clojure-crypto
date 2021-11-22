9626bb2125c68ec03f29c3cd02e3b821121cc017
Sébastien Le Callonnec <sebastien@weblogism.com>
2021-11-22 20:35:24 +0000

    Clojurify.

--- src/clojure_crypto/nssdb.clj
+++ src/clojure_crypto/nssdb.clj
@@ -3,37 +3,48 @@
            [java.io File])
   (:require [clojure-crypto.utils :as util]))
 
+(def SIG-ALGO "SHA256withECDSA")
 
-;; Uses Mozilla NSS to sign a message, and then to verify the signature.
-;; TODO Cleanup, make Clojure-y.
-(defn -main [& rest]
-
+(defn init-pkcs11-provider [conf]
   (let [proto (Security/getProvider "SunPKCS11")
-        config-path (.getAbsolutePath (File. "resources/config.cfg"))
+        config-path (.getAbsolutePath (File. conf))
         provider (.configure proto config-path)]
     (println "info:" (.getInfo provider))
     (Security/addProvider provider)
-    (KeyStore/getInstance "PKCS11" provider)
+    provider))
 
-    (let [ks (KeyStore/getInstance "PKCS11" provider)]
-      (.load ks nil (.toCharArray "password"))
-      (println "Count:" (.size ks))
+(defn load-keystore [provider password]
+  (let [ks (KeyStore/getInstance "PKCS11" provider)]
+    (.load ks nil (.toCharArray password))
+    (println "Count:" (.size ks))
+    ks))
 
-      (let [aliases (.aliases ks)]
-        (while (.hasMoreElements aliases)
-          (let [alias (.nextElement aliases)
-                cert (.getCertificate ks alias)]
-            (println cert)
-            (when (= "weblogism - Weblogism" alias)
-              (let [private-key (.getKey ks alias (.toCharArray "password"))
-                    sig (Signature/getInstance "SHA256withECDSA" provider)]
-                (.initSign sig private-key)
-                (.update sig (.getBytes "Hello World!"))
-                (println (.getEncoded private-key))
-                (let [signature (.sign sig)
-                      readable-sig (util/base64 signature)
-                      public-sig (Signature/getInstance "SHA256withECDSA" provider)]
-                  (println "Signature:" readable-sig)
-                  (.initVerify public-sig (.getPublicKey cert))
-                  (.update public-sig (.getBytes "Hello World!"))
-                  (println (.verify public-sig signature)))))))))))
+(defn get-certificate [keystore alias]
+  (.getCertificate keystore alias))
+
+(defn sign-message [msg provider keystore alias password]
+  (let [private-key (.getKey keystore alias (.toCharArray password))
+        sig (Signature/getInstance SIG-ALGO provider)]
+    (.initSign sig private-key)
+    (.update sig (.getBytes msg))
+    (.sign sig)))
+
+(defn verify-signature [msg signature provider cert]
+  (let [public-sig (Signature/getInstance SIG-ALGO provider)]
+    (.initVerify public-sig (.getPublicKey cert))
+    (.update public-sig (.getBytes msg))
+    (.verify public-sig signature)))
+
+
+;; Uses Mozilla NSS to sign a message, and then to verify the signature.
+;; See also https://magnus-k-karlsson.blogspot.com/2019/09/reading-nss-db-from-java-11-with.html
+(defn -main [& rest]
+  (let [msg "Hello World"
+        alias "weblogism - Weblogism"
+        provider (init-pkcs11-provider "resources/config.cfg")
+        keystore (load-keystore provider "password")
+        cert (get-certificate keystore alias)]
+    (println cert)
+    (let [signature (sign-message msg provider keystore alias "password")]
+      (println "Signature:" (util/base64 signature))
+      (println (verify-signature msg signature provider cert)))))
